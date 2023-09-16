0a93e788e4ddb74ec7ae42cfc58982c2e36965ba
LilianaC <liliana.castanon@gmail.com>
2023-09-16 14:09:24 -0600

    Update TestPlot.py

--- TestPlot.py
+++ TestPlot.py
@@ -10,10 +10,8 @@ df =pd.DataFrame({"source_number":[11199,11328,11287,32345,12342,1232,13456,1232
 
 columns = df.columns.tolist()
 
-#selected_columns = st.multiselect("select column",columns)
-#s = df[selected_columns].str.strip().value_counts()
-
 selected_columns = st.multiselect("select column", columns, default="location")
+
 s = df[selected_columns[0]].str.strip().value_counts()
 
 trace = go.Bar(x=s.index,y=s.values,showlegend = True)
@@ -21,3 +19,4 @@ layout = go.Layout(title = "test")
 data = [trace]
 fig = go.Figure(data=data,layout=layout)
 st.plotly_chart(fig)
+
