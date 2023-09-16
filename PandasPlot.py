b7a6a5416606fc9fb5965037c3e36f099fde1cff
LilianaC <liliana.castanon@gmail.com>
2023-09-16 14:20:02 -0600

    Update PandasPlot.py

--- PandasPlot.py
+++ PandasPlot.py
@@ -7,9 +7,11 @@ st.title('Gráfica de un dataframe: Seleccionando los datos')
 url = 'https://raw.githubusercontent.com/LilianaC/Pandas/master/Fifa%2023%20Fut%20Players.csv'
 df= pd.read_csv(url)
 
+st.title('Columna de barras')
+
 columns = df.columns.tolist()
 
-selected_columns = st.multiselect("select column", columns, default="League")
+selected_columns = st.multiselect("Selecciona la columna a graficar", columns, default="League")
 s = df[selected_columns[0]].str.strip().value_counts()
 
 trace = go.Bar(x=s.index,y=s.values,showlegend = True)
@@ -19,5 +21,17 @@ fig = go.Figure(data=data,layout=layout)
 st.plotly_chart(fig)
 
 
+st.title('Gráfica de dispersión')
+valx = st.multiselect("Selecciona los valores en x", columns, default="Country")
+valy = st.multiselect("Selecciona los valores en y", columns, default="Ratings")
+
+
+fig = px.scatter(
+    df,
+    x=valx,
+    y=valy,
+    size="pop",
+    color="continent"
+)
 
 
