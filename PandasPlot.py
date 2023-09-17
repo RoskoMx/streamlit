75d74c8b515d81d879a448d0a1b31af5af5b74fb
LilianaC <liliana.castanon@gmail.com>
2023-09-16 19:31:02 -0600

    Update PandasPlot.py

--- PandasPlot.py
+++ PandasPlot.py
@@ -39,7 +39,7 @@ st.text(valoresy.values)
 
 
 
-trace2 = px.line(df,x=valoresx.index,y=valoresy.values)
+trace2 = px.line(df,x=valoresx.values,y=valoresy.values)
 st.plotly_chart(trace2, theme="streamlit", use_container_width=True)
 
 #layout2 = go.Layout(title = "FIFA 21")
