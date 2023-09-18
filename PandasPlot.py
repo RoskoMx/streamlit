f45a8f0f7ac81f16674380a36b084d3f2365c415
LilianaC <liliana.castanon@gmail.com>
2023-09-17 18:45:18 -0600

    Update PandasPlot.py

--- PandasPlot.py
+++ PandasPlot.py
@@ -3,14 +3,16 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 
-st.title('Gráfica de un dataframe: Seleccionando los datos')
+#requirements.txt
+
+#Gráfica de barras
+st.title('Gráfica de un dataframe: Selección de datos por los usuarios')
 url = 'https://raw.githubusercontent.com/LilianaC/Pandas/master/Fifa%2023%20Fut%20Players.csv'
 df= pd.read_csv(url)
 
 st.title('Gráfica de barras')
 
 columns = df.columns.tolist()
-
 selected_columns = st.multiselect("Selecciona la columna a graficar", columns, default="League")
 s = df[selected_columns[0]].str.strip().value_counts()
 
@@ -20,35 +22,18 @@ data = [trace]
 fig = go.Figure(data=data,layout=layout)
 st.plotly_chart(fig)
 
-
+#Gráfica de dispersión
 st.title('Gráfica de Dispersión')
 valx = st.multiselect("Selecciona País, Liga o Club", columns, default="Country")
 valy = st.multiselect("Selecciona la métrica", columns, default="Ratings")
 
-
 valoresx = df[valx[0]]
 valoresy = df[valy[0]]
 
-st.text(type(valoresx))
-
-st.text(len(valoresx))
-st.text(len(valoresy))
-
-
-st.text(valoresx.index)
-st.text(valoresy.values)
-
-
-
-trace2 = px.scatter(df,x=valoresx.values,y=valoresy.values).update_yaxes(categoryorder="total descending")
-trace2.update_xaxes(type='category')
+trace2 = px.scatter(df,x=valoresx.values,y=valoresy.values)
 st.plotly_chart(trace2, theme="streamlit", use_container_width=True)
 
-#layout2 = go.Layout(title = "FIFA 21")
-#data2 = [trace2]
-#fig2 = go.Figure(data=data2,layout=layout2)
-#st.plotly_chart(fig2)
-
+#Multiselección gráfica de barras
 st.title('Gráfica de Dispersión')
 clist = df["Country"].unique().tolist()
 
