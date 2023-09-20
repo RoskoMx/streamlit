1cef31a32234d95c07f38f5dc23b56a798bc3938
LilianaC <liliana.castanon@gmail.com>
2023-09-19 18:36:14 -0600

    Update PandasPlot.py

--- PandasPlot.py
+++ PandasPlot.py
@@ -30,7 +30,7 @@ valy = st.multiselect("Selecciona la métrica", columns, default="Ratings")
 valoresx = df[valx[0]]
 valoresy = df[valy[0]]
 
-trace2 = px.line(df,x=valoresx.values,y=valoresy.values)
+trace2 = px.scatter(df,x=valoresx.values,y=valoresy.values)
 st.plotly_chart(trace2, theme="streamlit", use_container_width=True)
 
 #Multiselección gráfica de barras
