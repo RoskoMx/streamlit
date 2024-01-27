2a9bd1048f656ac06ba260aa5a6385f38951882b
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-01-27 08:22:38 -0600

    Update multipages.py

--- multipages.py
+++ multipages.py
@@ -112,29 +112,33 @@ def page4():
     st.session_state.name #Para mostrar lo que guardamos en key
     
     #Como un print y no es necesario escribir st.text
-    st.text('¡Hola '+st.session_state.name+' ! (Aquí concatenamos con st.text y con st.session_state.key') 
+    st.text('¡Hola '+st.session_state.name+' ! (Aquí concatenamos con st.text y con st.session_state.key)') 
     'Esta es otra forma de poner lo que pusimos en st.session_state.key, sin st.text, mi querid@ ',st.session_state.name
     
     #Generamos el Dataframe como si estuvieramos en Pandas
     'Importante generar el raw para el dataframe'
     df = pd.read_csv('https://raw.githubusercontent.com/LilianaC/streamlit3/main/Datos%20F1%20Dutch%20GP%20-%20Sheet1.csv')
     #Estos Dataframes se copian y pegan con la opción de raw, se pega la dirección de arriba del navegador
+    
+    
     #Sí el checkbox es verdadero y solo escribimos df
-    if st.checkbox('Mostrar dataframe'):
+    if st.checkbox('Mostrar dataframe (con el código st.checkbox en un if)'):
         df
     
     #Lista desplegable
     option = st.selectbox(
-        'Selecciona tu corredor favorito: ',
+        'Selecciona tu corredor favorito (con st.selectbox y dentro invocamos con df la casilla que va a llamar): ',
         #De dónde va a sacar la información
          df['DRIVER'])
     
     'Tu selección: ', option
     
     #Línea que muestra toda la info del renglón
+    'Mostramos todo lo que haya en un renglón de a cuerdo con: df.loc[df['Columna_Select']==variable]'
     df.loc[df['DRIVER'] == option]
     
     #Gráfica
+    'La gráfica aparece, de manera sencilla, con st.line_chart(df, x='AVG_Speed', y='LAP' )'
     st.line_chart(
         df,
         x = 'AVG SPEED',
