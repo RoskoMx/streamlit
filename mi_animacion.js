321dbf2154e24edfce08940f70869bfe2f8cc173
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-02-01 15:33:01 -0600

    Create mi_animacion.js

---
+++ mi_animacion.js
@@ -0,0 +1,16 @@
+// mi_animacion.js
+
+// Llama a la biblioteca LottieWeb
+import lottie from 'https://cdn.jsdelivr.net/npm/lottie-web/build/player/lottie_light.min.js';
+
+// Configuración de la animación
+var animacionConfig = {
+    container: document.getElementById('animation-container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'ruta_del_archivo/animacion.json' // Reemplaza con la ruta correcta a tu archivo JSON de animación Lottie
+};
+
+// Cargar la animación
+var animacion = lottie.loadAnimation(animacionConfig);
