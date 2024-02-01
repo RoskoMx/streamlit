4e7a485fa3de67f0cba030c896e397633d11137e
RoskoMx <78753834+RoskoMx@users.noreply.github.com>
2024-02-01 12:10:20 -0600

    Update multipages.py

--- multipages.py
+++ multipages.py
@@ -50,7 +50,7 @@ def Home():
                 ***Si esto suena interesante para ti puedes contactarnos a través del formulario que encontrarás al final de la página*** 
                 """
             )
-            st.write("[Más sobre nosotros>]https://debutants-sur-internet.streamlit.app/")
+            st.write(f"[{Más sobre nosotros}] ({https://debutants-sur-internet.streamlit.app})")
 
         with right_column:
             #st_lottie(load_lottieurl(lottie_file), height=400)
